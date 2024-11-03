a5dfa3798df248bc6a31a6b95710548492bbb4aa
NoahGX <noahjgarcia2019@gmail.com>
2024-11-03 13:28:51 -0800

    Update

--- kafka_streams.py
+++ kafka_streams.py
@@ -1,4 +1,11 @@
 import uuid
+import requests
 from airflow import DAG
 from datetime import datetime
 from airflow.operators.python import PythonOperator
+
+def get_data():
+    res = requests.get("https://randomuser.me/api/")
+    res = res.json()
+    res = res['results'][0]
+    return res
