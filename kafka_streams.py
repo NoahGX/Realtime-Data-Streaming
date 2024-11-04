c0bb5f4a0eb0510c842895b455e610b2633dfca6
NoahGX <noahjgarcia2019@gmail.com>
2024-11-04 02:04:53 -0800

    Update

--- kafka_streams.py
+++ kafka_streams.py
@@ -1,11 +1,11 @@
 import uuid
+import json
+import time
+import logging
 import requests
-    import json
-    import time
-    import logging
 from airflow import DAG
 from datetime import datetime
-    from kafka import KafkaProducer
+from kafka import KafkaProducer
 from airflow.operators.python import PythonOperator
 
 def get_data():
@@ -18,3 +18,21 @@ default_args = {
     'owner': 'airflow',
     'start_date': datetime(2024, 11, 3, 12, 00)
 }
+
+def format_data(res):
+    data = {}
+    location = res['location']
+    data['id'] = uuid.uuid4()
+    data['first_name'] = res['name']['first']
+    data['last_name'] = res['name']['last']
+    data['gender'] = res['gender']
+    data['address'] = f"{str(location['street']['number'])} {location['street']['name']}, " \
+                      f"{location['city']}, {location['state']}, {location['country']}"
+    data['post_code'] = location['postcode']
+    data['email'] = res['email']
+    data['username'] = res['login']['username']
+    data['dob'] = res['dob']['date']
+    data['registered_date'] = res['registered']['date']
+    data['phone'] = res['phone']
+    data['picture'] = res['picture']['medium']
+    return data
