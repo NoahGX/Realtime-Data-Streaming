2c19eb0376af2c8ed05bd1abeb1c7001a8a891ea
NoahGX <noahjgarcia2019@gmail.com>
2024-11-25 15:11:22 -0800

    Update

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,22 +1,31 @@
-#!/bin/zsh
-set -e
+#!/usr/bin/env bash
+set -euo pipefail
 
-if [ -e "/opt/airflow/requirements.txt" ]; then
+AIRFLOW_HOME="/opt/airflow"
+REQUIREMENTS_FILE="$AIRFLOW_HOME/requirements.txt"
+DB_FILE="$AIRFLOW_HOME/airflow.db"
+
+# Install requirements if 'requirements.txt' exists
+if [[ -f "$REQUIREMENTS_FILE" ]]; then
   python -m pip install --upgrade pip
-  python -m pip install --user -r /opt/airflow/requirements.txt
+  python -m pip install --user -r "$REQUIREMENTS_FILE"
 fi
 
-if [ ! -f "/opt/airflow/airflow.db" ]; then
+# Initialize an Airflow database
+# Create an admin user if the database does not exist
+if [[ ! -f "$DB_FILE" ]]; then
   airflow db init
   airflow users create \
     --username admin \
-    --password admin \
-    --email admin@example.com \
     --firstname admin \
     --lastname admin \
+    --password admin \
+    --email admin@example.com \
     --role Admin
 fi
 
+# Upgrade the Airflow database
 airflow db upgrade
 
+# Start the Airflow webserver
 exec airflow webserver
